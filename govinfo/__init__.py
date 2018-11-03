3cf3f91ba4cd305c4246a427938286b7185872a8
Forest Gregg <fgregg@uchicago.edu>
2018-11-03 14:45:13 -0500

    better organize end_time

--- govinfo/__init__.py
+++ govinfo/__init__.py
@@ -16,44 +16,40 @@ class GovInfo(scrapelib.Scraper):
         return response.json()
 
 
-    def _format_path(self, path, start_time, end_time):
+    def _format_time(self, dt)
 
-        utc_start_time = start_time.astimezone(pytz.utc)
-        start_time_str = utc_start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+        utc_time = dt.astimezone(pytz.utc)
+        time_str = dt.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-        utc_end_time = end_time.astimezone(pytz.utc)
-        end_time_str = utc_end_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+        return time_str
 
-        path = path.format(start_time=start_time_str,
-                           end_time=end_time_str)
-
-        return path
-
-
-    def congressional_hearings(self, start_time, end_time=None):
-        endpoint = '/collections/CHRG/{start_time}/{end_time}'
-
+    def congressional_hearings(self, start_time=None, end_time=None):
         if start_time is None:
-            if end_time is not None:
-                raise ValueError('if end_time is set so must start_time')
+            # the earliest date for this collection
+            start_time = datetime.datetime(2018, 6, 5, 0, 0, tzinfo=pytz.utc)
+        start_time_str = self._format_time(start_time)
 
         if end_time is None:
             end_time = datetime.datetime.now(pytz.utc)
+        end_time_str = self._format_time(end_time)
 
-        url = self.BASE_URL + self._format_path(endpoint, start_time, end_time)
+        partial = '/collections/CHRG/{start_time}'.format(start_time=start_time_str)
+        url_template = self.BASE_URL + partial + '/{end_time}'
 
         # we may need to check for duplicates here
-        for page in self._pages(url):
+        for page in self._pages(url_template, end_time_str):
             for package in page['packages']:
                 response = self.get(package['packageLink'])
                 yield response.json()
 
 
-    def _pages(self, url):
+    def _pages(self, url_template, end_time_str):
         page_size = 100
 
         first_page_params = {'offset':  0,
                              'pageSize': page_size}
+
+        url =  url_template.format(end_time=earliest_timestamp)
 
         response = self.get(url,
                             params=first_page_params)
@@ -61,14 +57,12 @@ class GovInfo(scrapelib.Scraper):
 
         yield data
 
-        url_template = url.lsplit('/', 1)[0] + '/{end_time}'
-
         while len(data['packages']) == page_size:
             # the API results are sorted in descending order by timestamp
             # so we can paginate through results by making the end_time
             # filter earlier and earlier
             earliest_timestamp = data['packages'][-1]['lastModified']
-            url =  url_template.format(end_time=earliest_timestamp)
+            url = url_template.format(end_time=earliest_timestamp)
 
             response = self.get(url,
                                 params=first_page_params)
